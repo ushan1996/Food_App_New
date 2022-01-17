2ae322f69aa7c1214df1c6f79d9be742f4bb74a3
ushan1996 <64035537+ushan1996@users.noreply.github.com>
2022-01-17 12:27:54 +0530

    Splash Screen addred

---
+++ lib/screens/splash_screen/getting_started.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GettingStart extends StatelessWidget {
+  const GettingStart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/screens/splash_screen/getting_started.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => GettingStart()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Container(
+      width: Size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/food.png'),
+          const SizedBox(height: 8),
+          const Text(
+            "No Wating For Foods",
+            style: TextStyle(
+              fontSize: 16,
+              color: Color(0xff838383),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
